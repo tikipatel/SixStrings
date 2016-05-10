91944c1adbf3bee3c7aed25282bbb69bb44c75e1
Sean Johnsen <sean@joyces-imac>
2016-05-10 14:27:41 -0500

    Initial Commit

---
+++ SixStrings/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SixStrings
+//
+//  Created by sean on 5/10/16.
+//  Copyright © 2016 Sean Johnsen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
